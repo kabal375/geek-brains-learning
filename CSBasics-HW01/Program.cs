2c79784ce3cfda4e77cfd52941967f10e2a7df49
Konstantin Kasachev <k.kasachev@gmail.com>
2022-10-01 12:53:55 +0300

    HW1 Task1 - max of two numbers

--- CSBasics-HW01/Program.cs
+++ CSBasics-HW01/Program.cs
@@ -1,14 +1,36 @@
-﻿Console.Clear();
-
-Console.WriteLine("1 - Найти максимум из двух чисел");
-Console.WriteLine("2 - Найти максимум из трёх чисел");
-Console.WriteLine("3 - Является ли число чётным");
-Console.WriteLine("4 - Чётные числа от 1 до N");
-Console.WriteLine("5 - Выход");
-
+﻿
 int choice = 0;
 
 while (choice != 5) {
+    showMenu();
     choice = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Ok");
+
+    if (choice == 1) {
+        maxOfTwo();
+    }
+}
+
+void showMenu() {
+    Console.Clear();
+
+    Console.WriteLine("1 - Найти максимум из двух чисел");
+    Console.WriteLine("2 - Найти максимум из трёх чисел");
+    Console.WriteLine("3 - Является ли число чётным");
+    Console.WriteLine("4 - Чётные числа от 1 до N");
+    Console.WriteLine("5 - Выход");
+}
+
+
+void maxOfTwo() {
+    //
+    int n1 = Convert.ToInt32(Console.ReadLine());
+    int n2 = Convert.ToInt32(Console.ReadLine());
+    if (n1 > n2) {
+        Console.WriteLine("max = " + n1);
+    } else if (n1 < n2) {
+        Console.WriteLine("max = " + n2);
+    } else Console.WriteLine("Числа равны");
+    Console.WriteLine("Нажмите Enter для продолжения");
+    Console.ReadLine();
 }

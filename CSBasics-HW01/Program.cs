7b40cfe2833c79d6cd4ae883d47b62032ac62be2
Konstantin Kasachev <k.kasachev@gmail.com>
2022-10-01 12:59:15 +0300

    HW1 Task1 cosmetics

--- CSBasics-HW01/Program.cs
+++ CSBasics-HW01/Program.cs
@@ -24,7 +24,9 @@ void showMenu() {
 
 void maxOfTwo() {
     //
+    Console.WriteLine("Введите первое число:");
     int n1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число:");
     int n2 = Convert.ToInt32(Console.ReadLine());
     if (n1 > n2) {
         Console.WriteLine("max = " + n1);
